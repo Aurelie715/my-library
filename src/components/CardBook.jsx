25a7a4deb30200bb714e51a30f53c391a9dd97ab
Aurelie715 <aureliebernard29@gmail.com>
2023-11-07 17:37:40 +0100

    add bookList json file

--- src/components/CardBook.jsx
+++ src/components/CardBook.jsx
@@ -3,13 +3,13 @@ import styles from './CardBook.module.scss'
 
 export default function CardBook() {
   return (
-    <div className={styles.card}>
+    <article className={styles.card}>
       <img src="./panique-a-la-ferme.jpg" alt="" />
       <div className={styles["card-info"]}>
         <p className={styles.title}>Panique à la ferme</p>
         <p className={styles.author}>Christine DORNER</p>
       </div>
 
-    </div>
+    </article>
   )
 }
