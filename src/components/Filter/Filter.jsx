d368523fd3e8bb1facbf570d5ab53a7a8d072715
Aurelie715 <aureliebernard29@gmail.com>
2023-11-20 16:29:16 +0100

    add style to filter component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import styles from './Filter.module.scss';
 
 export default function Filter({buttonName, element1, element2}) {
   return (
-    <div>
+    <div className={styles.dropdown}>
         <button type="button">
-            {buttonName}<FontAwesomeIcon icon={faAngleDown} />
+            {buttonName}<FontAwesomeIcon icon={faAngleDown} className={styles.icon}/>
         </button>
         <ul>
             <li>{element1}</li>
